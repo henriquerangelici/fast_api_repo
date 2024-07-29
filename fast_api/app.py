a830f7390f3d4a05f7b5c4f9ea93ed90ac163d5c
Henrique Angelici <henrique.angelici@gmail.com>
2024-07-28 21:21:57 -0300

    Implementando CRUD

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,12 +1,54 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from fast_api.schemas import Message
+from fast_api.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'ola mundo'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    # o user vai p/ UserSchema (validação), mas retorno é o UserPublic
+    user_id = UserDB(
+        id=len(database) + 1,
+        **user.model_dump(),  # convert pydantic em dict
+    )
+
+    database.append(user_id)
+
+    return user_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{id_user}', response_model=UserPublic)
+def update_user(id_user: int, user: UserSchema):
+    if id_user < 1 or id_user > len(database):
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Usuário não encontrado')
+
+    user_id = UserDB(**user.model_dump(), id=id_user)
+
+    database[id_user - 1] = user_id
+
+    return user_id
+
+
+@app.delete('/users/{id_user}', response_model=Message)
+def delete_user(id_user: int):
+    if id_user < 1 or id_user > len(database):
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='Usuário não encontrado')
+
+    del database[id_user - 1]
+
+    return {'message': 'Usuario deletado'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,55 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_api.app import app
-
-
-def test_read_root_deve_retornar_ok_e_olamundo():
-    client = TestClient(app)  # Arrange (Organização)
-
+def test_read_root_deve_retornar_ok_e_olamundo(client):
     response = client.get('/')  # Act (ação)
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'ola mundo'}  # Assert
+
+
+def test_create_user(client):
+    response = client.post(  # User Schema
+        '/users/',
+        json={
+            'username': 'testename',
+            'password': 'senha',
+            'email': 'teste@teste.com',
+        },
+    )
+
+    # Validar UserPublic
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {'username': 'testename', 'email': 'teste@teste.com', 'id': 1}
+
+
+def test_read_user(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'testename',
+                'email': 'teste@teste.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_ser(client):
+    response = client.put(
+        '/users/1',
+        json={'username': 'testename2', 'email': 'teste@teste.com', 'id': 1, 'password': '123'},
+    )
+
+    assert response.json() == {
+        'username': 'testename2',
+        'email': 'teste@teste.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+    assert response.json() == {'message': 'Usuario deletado'}
